6ec79e6a9997d360414aca7241a18c72bccd3783
llanik2 <llanik2@huskers.unl.edu>
2020-04-25 14:59:36 -0500

    done!

--- WeatherCalculator2/WeatherCalculator2/Program.cs
+++ WeatherCalculator2/WeatherCalculator2/Program.cs
@@ -10,6 +10,8 @@ namespace WeatherCalculator2
 
             int temperature;
             int windSpeed;
+            int RelHumidity;
+            double HeatInd;
             double windChill;
             string input = "";
 
@@ -20,18 +22,32 @@ namespace WeatherCalculator2
 
             temperature = int.Parse(input);
 
-            // Get the windspeed from the user
-            Console.WriteLine("Enter the wind speed");
+            // Detect if over 50 degrees
+            if(temperature > 50)
+            {
+                Console.WriteLine("Enter the relitive humidity");
 
-            input = Console.ReadLine();
+                input = Console.ReadLine();
 
-            windSpeed = int.Parse(input);
+                RelHumidity = int.Parse(input);
 
-            // Calculate the wind chill
+                HeatInd = .5 * (temperature + 61 + ((temperature - 68) * 1.2) + (RelHumidity * 0.094));
+            }
+            else
+            {
+                // Get the windspeed from the user
+                Console.WriteLine("Enter the wind speed");
 
-            windChill = 35.74 + (0.6125 * temperature) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
+                input = Console.ReadLine();
 
-            Console.WriteLine("The wind chill is " + windChill);
+                windSpeed = int.Parse(input);
+
+                // Calculate the wind chill
+
+                windChill = 35.74 + (0.6125 * temperature) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
+
+                Console.WriteLine("The wind chill is " + windChill);
+            }
         }
     }
 }
